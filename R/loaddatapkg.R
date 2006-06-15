12125b07740b0e7159a6ce018d92c23289f158e7
Jorn Todling <toedling@ebi.ac.uk>
2006-06-15 17:24:48 +0000

    removed 'reposTools' dependency
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/macat@18451 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/loaddatapkg.R
+++ R/loaddatapkg.R
@@ -2,18 +2,7 @@ loaddatapkg <- function(mydatapkg,installDir=.libPaths()[1]){
   if (mydatapkg %in% .packages(all.available=TRUE)) {
     library(mydatapkg,character.only=TRUE)
   } else {
-    if ("reposTools" %in% .packages(all.available=TRUE)) {
-      library(reposTools)
-      myrepos <- getReposEntry("http://compdiag.molgen.mpg.de/software/Rrepos")
-      insttry <- try(install.packages2(mydatapkg,myrepos,lib=installDir),silent=TRUE)      
-      libtry <- try(library(mydatapkg,character.only=TRUE),silent=TRUE)
-      if ((class(insttry)=="try-error")|(class(libtry)=="try-error")){
-        stop(paste("Couldn't load data package '",mydatapkg,"'!\nPlease, try to obtain it from\n\nhttp://compdiag.molgen.mpg.de/software \n\nand install it yourself.\n",sep=""),call.=FALSE)
-      } # if (class(insttry)=="try-error")
-    } else { # no reposTools
-      stop(paste("Need Package 'reposTools' for automatic download. !\nPlease, try to obtain package '",mydatapkg,"' manually from \n\nhttp://compdiag.molgen.mpg.de/software\n\n install it and rerun this demo.\n",sep=""),call.=FALSE)
-    } # else if no reposTools
+    install.packages(mydatapkg, lib=installDir, contriburl=contrib.url("http://www.ebi.ac.uk/huber-srv/data/Rrepos"))
+    library(mydatapkg,character.only=TRUE)
   } # else package not available
 } # loaddatapkg
-
-
