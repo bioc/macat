1e0be0ff7a115cd875f6030f3bf76c5b4bf50603
Jorn Todling <toedling@ebi.ac.uk>
2006-06-28 10:10:34 +0000

    removed data package from 'Suggests' since this produces unwarranted errors in the check report
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/macat@18640 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/preprocessedLoader.R
+++ R/preprocessedLoader.R
@@ -12,20 +12,22 @@
 # trailing columns hold the expressionlevels for all samples
 ###############################################################################
 
-preprocessedLoader <- function(rdatafile,chip,labels=NULL,rdafile=TRUE,tabfile=FALSE,labelfile=FALSE) {
-  # file: is first argument  a file or the expression matrix
-  require(annotate)
-  #acceptedChips <-  c("hu6800","hgu95av2","hgu95b","hgu95c","hgu95d","hgu95e",
-  #                    "hgu133a","hgu133b","mgu74av2","mgu74bv2","mgu74c",
-  #                    "moe430a","moe430b","rae230a","rae230b","rgu34a",
-  #                    "rgu34b","rgu34c")
-  loadChip <- require(chip, character.only=TRUE, quiet=TRUE)
-  if (!loadChip){
-    stop(paste("Unknown Chip! At present, MACAT can only deal with chips, for which there",
-               "exists an installed BioConductor annotation data package, such as 'hgu95av2'.",
-               "The chip type must be specified using the name of this annotation package,",
-               "e.g., for the Human Genome 95 A version 2 chip: 'hgu95av2'.\n", sep="\n"))
-  }
+preprocessedLoader <- function(rdatafile,chip,labels=NULL,chromLocObj=NULL, rdafile=TRUE,tabfile=FALSE,labelfile=FALSE) {
+  # rdafile: is first argument  a file or the expression matrix
+
+  # instead of chip name and building chromLocation object from it, can also provide
+  #  chromLocation object directly
+  if (!is.null(chromLocObj)){
+    stopifnot(inherits(chromLocObj,"chromLocation"))
+  } else {
+    loadChip <- require(chip, character.only=TRUE, quiet=TRUE)
+    if (!loadChip){
+      stop(paste("Unknown Chip! At present, MACAT can only deal with chips, for which there",
+                 "exists an installed BioConductor annotation data package, such as 'hgu95av2'.",
+                 "The chip type must be specified using the name of this annotation package,",
+                 "e.g., for the Human Genome 95 A version 2 chip: 'hgu95av2'.\n", sep="\n"))
+    }
+  }#else (!is.null(chromLocObj)
   cat("Reading expression matrix...\n")
   if ((!rdafile)&(!tabfile)) # is m already a matrix?
     m <- as.matrix(rdatafile)
@@ -51,7 +53,10 @@ preprocessedLoader <- function(rdatafile,chip,labels=NULL,rdafile=TRUE,tabfile=F
   } # if (!is.null(labels))
                     
   cat("Assessing chromosome information for genes on chip...\n")
-  chromLocationObj <- buildChromLocation(chip)
+  if (is.null(chromLocObj))
+    chromLocationObj <- buildChromLocation(chip)
+  else
+    chromLocationObj <- chromLocObj
   if (is.null(colnames(m))){
     colnames(m) <- paste("sample",as.character(1:ncol(m)),sep="")
   }
@@ -61,7 +66,7 @@ preprocessedLoader <- function(rdatafile,chip,labels=NULL,rdafile=TRUE,tabfile=F
   pdata <- new("phenoData", pData=sampleDataFrame,
                varLabels=list(names(sampleDataFrame)))
   eset  <- new("exprSet", exprs=m, phenoData=pdata)
-  chromosomes <- names(chromLocationObj@chromInfo)                  
+  chromosomes <- names(chromLocs(chromLocationObj))
   allGeneNames <- c()
   allGeneLocations <- c()
   allChromosomes <- c()
@@ -76,7 +81,7 @@ preprocessedLoader <- function(rdatafile,chip,labels=NULL,rdafile=TRUE,tabfile=F
     usedGenes <- as.list(usedGenes[ord])
     return(usedGenes)
   }#usedChromGenes2
-  
+
   for (chromosome in chromosomes) {
     cat(paste("Locating genes on chromosome",chromosome,".... "))
     usedGenes <- usedChromGenes2(eset, chromosome, chromLocationObj)
@@ -94,7 +99,7 @@ preprocessedLoader <- function(rdatafile,chip,labels=NULL,rdafile=TRUE,tabfile=F
     allChromosomes <- c(allChromosomes,as.character(rep(chromosome,length(geneName))))
   } # for chromosome
   if (length(allGeneNames)==0)
-    stop("Gene-Identifier not found on specified chip!\nIf Affymetrix-Chip,Probe-Set-IDs are expected!")
+    stop("Gene-Identifier not found on specified chip!\nCheck 'featureNames' of the ExpressionSet or 'rownames' of the matrix.\nFor Affymetrix-Arrays, Probe-Set IDs are expected!")
   data <- list(geneName=allGeneNames,geneLocation=allGeneLocations,
                chromosome=allChromosomes, expr=m,labels=labels,chip=chip)
   class(data) <- "MACATData"
@@ -102,12 +107,17 @@ preprocessedLoader <- function(rdatafile,chip,labels=NULL,rdafile=TRUE,tabfile=F
 } # preprocessedLoader
 
 ## wrapper for preprocessedLoader: ###
-buildMACAT <- function(matrix,chip,labels=NULL){
-  if (class(matrix)=="exprSet")
+buildMACAT <- function(matrix,chip,labels=NULL, chromLocObj=NULL){
+  if (class(matrix)=="ExpressionSet"){
+    rNames <- featureNames(matrix)
+    cNames <- sampleNames(matrix)
     matrix <- exprs(matrix)
+    rownames(matrix) <- rNames
+    colnames(matrix) <- cNames
+  }
   if (!is.matrix(matrix)) stop("First argument is no matrix!\n")
-  return(preprocessedLoader(matrix,chip,labels,rdafile=FALSE,
-                            tabfile=FALSE,labelfile=FALSE)) 
+  return(preprocessedLoader(matrix,chip,labels,chromLocObj=chromLocObj,
+                            rdafile=FALSE,tabfile=FALSE,labelfile=FALSE)) 
 } #buildMACAT
 
 
