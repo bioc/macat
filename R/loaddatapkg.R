c9a612bedcfdf5336f3491f5fa116b7da433a716
Jorn Todling <toedling@ebi.ac.uk>
2006-09-15 13:01:25 +0000

    updated access of remote data package to deal with warning message; clarified help page of function evaluateParameters
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/macat@19851 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/loaddatapkg.R
+++ R/loaddatapkg.R
@@ -1,8 +1,8 @@
 loaddatapkg <- function(mydatapkg,installDir=.libPaths()[1]){
   if (mydatapkg %in% .packages(all.available=TRUE)) {
-    library(mydatapkg,character.only=TRUE)
+    require(mydatapkg,character.only=TRUE)
   } else {
     install.packages(mydatapkg, lib=installDir, contriburl=contrib.url("http://www.ebi.ac.uk/huber-srv/data/Rrepos"))
-    library(mydatapkg,character.only=TRUE)
+    require(mydatapkg,character.only=TRUE)
   } # else package not available
 } # loaddatapkg
