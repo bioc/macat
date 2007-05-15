6dbe3135a0f3c16ae83bfe986e2888842073782a
Jorn Todling <toedling@ebi.ac.uk>
2007-05-15 16:23:28 +0000

    added NAMESPACE and CITATION files
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/macat@24663 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,21 @@
-.First.lib <- function(libname, pkgname){
+.onLoad <- function(libname, pkgname) {
+  ## nothing to do here for the moment
+}
+
+.onAttach <- function(libname, pkgname) {
   cat("Loading MicroArray Chromosome Analysis Tool...\n")
   cat("Loading required packages...\n")
-  require(Biobase,quietly=TRUE)
-  require(annotate,quietly=TRUE)
+  
   if (!("stjudem" %in% .packages(all.available=TRUE))){
     cat("You need package 'stjudem', if you want to see the demo and examples.\nType 'loaddatapkg(\"stjudem\")' for automatic install!\n\n")
   } # if data package not installed
   cat("Type 'demo(macatdemo)' for a quick tour...\n")
-  
+  ## show vignette in windows menu  
   if(.Platform$OS.type=="windows" && require(Biobase) && interactive() && .Platform$GUI=="Rgui"){
     addVigs2WinMenu("macat")
   }
-} # .First.lib
+} # .onAttach
 
+.onUnload <- function( libpath ) {
+  ## nothing to do here for the moment
+} # .onUnload
